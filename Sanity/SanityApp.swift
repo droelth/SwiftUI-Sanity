d95afd063f2fb59b59ac3da10baad13d9082251f
Berkay <berkaysutlu94@gmail.com>
2023-10-27 13:18:14 +0300

    Initial Commit

---
+++ Sanity/SanityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanityApp.swift
+//  Sanity
+//
+//  Created by Berkay Sutlu on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SanityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
